4d634eb7b98858d535def5ff1270ca7d555e13c8
FredViken <fredrik.mjos@hotmail.com>
2022-09-11 14:25:17 +0200

    Create index.js and gitignore

---
+++ index.js
@@ -0,0 +1,50 @@
+const puppeteer = require('puppeteer');
+
+
+exports.getStreams = (url) => {
+    data = [];
+    (async () => {
+        const browser = await puppeteer.launch({
+            headless: true
+        })
+        const page = await browser.newPage()
+    
+        
+        await page.goto(url, {
+            waitUntil: 'load'
+        });
+
+        page.on('response', async (response) => {
+            try{
+                const album = await response.json();
+                
+                if ('name' in album.data.album.tracks.items[0].track){
+                    const tracks = album.data.album.tracks.items
+                    
+                    
+    
+                    tracks.forEach(element => {
+                        console.log(element.track.name + ' - ' + element.track.playcount);
+                        data.push({
+                            name: element.track.name,
+                            streams: element.track.playcount
+                        })
+                    });
+                    
+                    browser.close()
+                    console.log(data);
+                    return data
+    
+                
+                }
+    
+            } catch (err){
+               
+            }
+    
+                
+        });
+        
+    })().catch(err => console.error(err))
+    
+}
