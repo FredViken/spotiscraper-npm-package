20c47261f6c70228c3ba9b18825848255cf8df4e
FredViken <fredrik.mjos@hotmail.com>
2022-10-20 20:10:22 +0200

    removed console log

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-extra')
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin())
 
 exports.getStreams = async (url, callback) => {
     let data = [];
-    let done = false;
 
-    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
-    const page = await browser.newPage();
+    const browser = await puppeteer.launch({headless:true, args: ['--no-sandbox', '--disable-setuid-sandbox']});
+    const page = await browser.newPage(); 
     await page.goto(url, {waitUntil: 'load'});
+    //console.log('Succesfully loaded page');
+    //console.log(page.url());
 
     page.on('response', async (response) => {
         try{
             const album = await response.json();
-            if ('name' in album.data.album.tracks.items[0].track){
-                const tracks = album.data.album.tracks.items
-                    
+            /*try{
+                console.log(album);
+            } catch {
+                console.log('nope');
+            }*/
+            if ('name' in album.data.albumUnion.tracks.items[0].track){
+                const tracks = album.data.albumUnion.tracks.items
+                
                 tracks.forEach((element,key) => {
                     
                     data.push({
@@ -26,12 +34,19 @@ exports.getStreams = async (url, callback) => {
                 })
 
                 browser.close();
+                //console.log('Closing headless browser');
                 callback(data)
                 
             }
         } catch {
-
+            
         }
     })
         
 }
+
+//For testing purposes
+
+// exports.getStreams('https://open.spotify.com/album/21jF5jlMtzo94wbxmJ18aa', (result) => {
+//     console.log(result);
+// })
